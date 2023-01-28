d68d5e3c551e530a69dc8f9f10e52a69758bf2cf
Mukhammad_Azam_001 <rizayevmuhammadazam9@gmail.com>
2023-01-28 03:45:19 +0300

    initial commit

---
+++ js/index.js
@@ -0,0 +1,97 @@
+// let massa = prompt("massangizni kiriting");
+// let buyi = prompt("buyinggizni kiriting");
+// let massa1 = prompt("buyinngizni kiring");
+// let buyi2 = prompt("massangizni kiriting");
+// const bmimassa = massa / buyi ** 2;
+// const bmimassa1 = massa1 / buyi2 ** 2;
+// if (bmimassa > bmimassa1) {
+//     alert(bmimassa)
+// } else {
+//     alert(bmimassa1)
+// }
+
+// function calculateBmi(name, weight, height) {
+//         let bmi = weight / (height * height);
+//         console.log(name + "'s BMI is " + bmi);
+//         console.log(`${name}'s BMI is ${bmi} `);}
+// let numberOne = 70,
+//     numberTwo = 60;
+// if (numberOne > numberTwo) {
+//     console.log("Kattasi:", numberOne);
+//     console.log("Kichigi", numberTwo);
+// } else if (numberOne < numberTwo) {
+//     console.log("Kattasi:", numberTwo);
+//     console.log("Kichigi", numberOne);
+// } else {
+//     console.log("Ikkala sson ham teng");
+// }
+// UYGA VAZIFA LESSON28
+// const nusim = prompt("massangizni kiriting");
+// const nusib = prompt("buyingizni kiriting");
+// const abdum = prompt("massangizni kiriting");
+// const abdub = prompt("buyingizni kiriting");
+// const BMInusi = (nusim / nusib ** 2);
+// const BMIAbdu = (abdum / abdub ** 2);
+// const ayirma = (BMInusi - BMIAbdu)
+// console.log(`nussining bmi ${BMInusi} abduning bmi ${BMIAbdu} VA nussi shuncha ${ayirma} ogir ekan`)
+// const myage = prompt("yoshingizni kiriting");
+// if (myage <= 18) {
+//     console.log("siz hali uqishingiz kerak")
+// } else if (myage <= 60) {
+//     console.log("sizz mexnat qilishengiz kerak")
+// } else {
+//     console.log("siz pensiya yoshidasiz ")
+// }
+// function weekday() {
+//     let today = 3;
+//     switch (today) {
+//         case 1:
+//             console.log("dushanba");
+//             break;
+//         case 2:
+//             console.log("seshanba");
+//             break;
+//         case 3:
+//             console.log("chorshanba");
+//             break;
+//         case 4:
+//             console.log("payshanba");
+//             break;
+//         case 5:
+//             console.log("juma");
+//             break;
+//         case 6:
+//             console.log("shanba");
+//             break;
+//         case 7:
+//             console.log("yakshanba")
+//             break;
+//         default:
+//             console.log("bazada avjud emas");
+//             break;
+//     }
+
+
+// }
+// weekday()
+// let son = prompt();
+// if (son % 15 === 0) {
+//     console.log("15ga bulinadi");
+
+// } else if (son % 5 === 0) {
+//     console.log("5ga bulinadi");
+// } else if (son % 3 === 0) {
+//     console.log("3gabulinadi");
+
+// } else {
+//     console.log({ son });
+// }
+
+// function son(input) {
+//     if (!(input % 15)) return "15gabulinadi";
+//     else if (!(input % 3)) return "3ga bulinadi";
+//     else if (!(input % 5)) return "5ga bulinadi";
+//     else return input;
+// }
+// const output = son();
+// console.log(output);
